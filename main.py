26bf8d0b31b87c3492873f23e5f2b17b1360fc90
Plaudible <48495874+Plaudible@users.noreply.github.com>
2020-02-15 23:50:24 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,52 +2,69 @@ import praw
 import time
 import re
 from praw.models import Comment
+from imgurpython import ImgurClient
+from fryer import doBot
+# from auth import authenticate
 
 f = open("api.txt", "r")  # api values stored in "api.txt."
 api_id = (f.readline())  # id line
 api_secret = (f.readline())  # secret line
-api_id = api_id[:-1]
+api_id = api_id[:-1] 
 api_secret = api_secret[:-1]
 
-reddit = praw.Reddit(client_id=api_id,
+reddit = praw.Reddit(client_id=api_id,#reddit api initialization
                      client_secret=api_secret,
                      username=HIDDEN,
                      password=HIDDEN,
-                     user_agent='<console:reddit_bot:0.0.1')
+                     user_agent=HIDDEN)
 
 
 def search():
-    unread_mentions = []
+    unread_mentions = [] #keeps track of the unread messages
 
-    for message in reddit.inbox.unread(limit=None):
+    for message in reddit.inbox.unread(limit=None): #marks read messages to prevent repeats
         subject = message.subject.lower()
         if subject == 'username mention' and isinstance(message, praw.models.Comment):
             unread_mentions.append(message)
             message.mark_read()
 
-    for x in unread_mentions:
-        submission = x.submission.url
+    for x in unread_mentions: #parse submission
+        submission = x.submission.url 
         subparse = str(submission)
-        if str(x.body) == '/u/lazer_eye_bot':
+        if str(x.body) == '/u/lazer_eye_bot': #post input handling, no image link
             if ".png" in subparse or ".jpg" in subparse or ".jpeg" in subparse:
                 print(submission)
                 result = str(process(submission))
-                # x.reply(result) RESPOND WITH OUTPUT IMAGE
+                x.reply(result)
                 print("Link posted!")
             else:
-                # x.reply('The format of this post does not support lazer eyes :(')
+                x.reply('The format of this post does not support laser eyes :( .jpg or .png please!')
                 print("Unsupported format!")
-        else:
+        else: #image link input handling
             urlfinder = str(x.body)
             print(re.search("(?P<url>https?://[^\s]+)", urlfinder).group("url"))
             result = process(urlfinder)
-            # x.reply(result) #RESPOND WITH OUTPUT IMAGE
+            x.reply(result)
 
 def process(url):
-    print('Processing...')
-    finalurl = "whatever" # DO IMAGE PROCESSING HERE
-    return(finalurl)
 
-while True:
-    search()
-    time.sleep(5)
+    imgup = doBot(url) #imgur API initialization and authentication
+    imgup.save('godhelpusall.jpg')
+    client = ImgurClient(HIDDEN,HIDDEN)
+    config = { #upload descriptions
+        'album': None,
+        'name': 'Deep fried',
+        'title': 'Delicious',
+        'description': 'This image is officially blessed'
+    }
+
+    print("Uploading image... ")
+    finalurl = str(client.upload_from_path('godhelpusall.jpg', config=config, anon=False)) #upload image
+    finalurl = re.search("(?P<url>https?://[^\s]+)", finalurl).group("url") #remove non-URL clutter
+    print("Done")
+    return(finalurl) #return the final url to message reply above
+
+if __name__ == "__main__":
+    while True:
+        search() #REMINDER: with not enough karma, it can only return an output every 10 minutes!
+        time.sleep(5) #sleep to avoid exceeding rate limit
