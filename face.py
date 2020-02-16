fc964967d9a4e6538635b898f4d14698412c516b
Guoliang Chen <luciochen233@gmail.com>
2020-02-15 20:27:13 -0500

    added face file

---
+++ face.py
@@ -0,0 +1,88 @@
+import cv2
+
+cascPath = "./data/haarcascade_frontalface_alt.xml"
+eyePath = "./data/haarcascade_eye_tree_eyeglasses.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier(eyePath)
+
+testImage = "matt.jpg"
+
+
+def show_camera():
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        # frame = cv2.imread(testImage)
+
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=0
+        )
+
+        # eye = eyeCascade.detectMultiScale(
+        #     gray,
+        #     scaleFactor=1.1,
+        #     minNeighbors=5,
+        #     minSize=(30, 30),
+        #     flags=0
+        # )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            faceROI = gray[y:y+int(h/2), x:x+w]
+            eyes = eyeCascade.detectMultiScale(faceROI)
+            for (x2, y2, w2, h2) in eyes:
+                eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+                radius = int(round((w2 + h2)*0.25))
+                frame = cv2.circle(frame, eye_center, radius, (255, 0, 0), 4)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        scale_percent = 100  # percent of original size
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
+        dim = (width, height)
+
+        frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+
+def get_face(image):
+    eye_list = []
+    frame = cv2.imread(image)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=0
+    )
+    for (x, y, w, h) in faces:
+        faceROI = gray[y:y+int(h/2), x:x+w]
+        eyes = eyeCascade.detectMultiScale(faceROI)
+        for (x2, y2, w2, h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            eye_radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, eye_center, eye_radius, (255, 0, 0), 4)
+            
+            eye_list.append(tuple(list(eye_center)+[eye_radius]))
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return eye_list
+
+
+show_camera()
+eye_list = get_face(testImage)
+print(eye_list)
+
